8c6504d7db2536600e24320ef4a1c2df173fc008
marianaakuma <153663415+marianaakuma@users.noreply.github.com>
2025-05-21 15:18:23 +0000

    1

---
+++ bd.py
@@ -0,0 +1,20 @@
+Carros = [
+    {
+        'id': 1,
+        'marca': 'fiat',
+        'modelo': 'mareta',
+        'ano': 1999
+    },
+    {
+        'id': 2,
+        'marca': 'fiat',
+        'modelo': 'uno',
+        'ano': 1999
+    },
+    {
+        'id': 3,
+        'marca': 'ford',
+        'modelo': 'escort',
+        'ano': 1985
+    }
+]
